d2ca1048b5e2e93a6ed7632a605b1319a930fe58
Issac Moreno <issaxavier909@gmail.com>
2020-05-20 15:19:15 -0700

    Submit button renders car component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import './App.css';
 import Dropdown from './components/search';
+import Results from './components/results';
 
 function App() {
   return (
     <div className="App">
       <Dropdown />
+      <Route path = '/:make/:model' component = {Results} />
     </div>
   );
 }
---
+++ src/components/results/index.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import axios from 'axios';
+
+import { useParams } from "react-router";
+
+const Results = () => {
+  const { make, model } = useParams();
+
+  const fetchSelectedCarData = async () => {
+    return await axios.get(
+      `https://streetsmarts-labs24.herokuapp.com/api/cars/?make=${make}&model=${model}`
+    );
+  };
+
+  useEffect(() => {
+    fetchSelectedCarData()
+      .then((res) => {
+        console.log("This is response of selected car data", res);
+      })
+      .catch((err) => {
+        console.log("Error in response of selected car data", err);
+      });
+  });
+
+  return (
+    <div>
+      <p>{make}</p>
+      <p>{model}</p>
+    </div>
+  );
+};
+
+export default Results;
