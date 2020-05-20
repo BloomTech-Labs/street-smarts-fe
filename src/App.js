01eb0e4b55d698515074a4ab06f175c731502580
Issac Moreno <issaxavier909@gmail.com>
2020-05-20 12:07:28 -0700

    Models are dependent on Makes now

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { ReactQueryDevtools } from "react-query-devtools";
-
 
 import './App.css';
 import Dropdown from './components/search';
-console.log("ANYTHING")
+
 function App() {
   return (
     <div className="App">
       <Dropdown />
-      <ReactQueryDevtools initialIsOpen={false} />
     </div>
   );
 }
--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,87 +1,106 @@
 import React, { useState, useEffect } from "react";
-// import axios from "axios";
+import axios from "axios";
+
 import "antd/dist/antd.css";
 import { Select } from "antd";
 const { Option } = Select;
 
+
+
+const fetchModelData = async (selection) => {
+  return await axios.get(
+    `https://streetsmarts-labs24.herokuapp.com/api/model?make=${selection}`
+  );
+};
+
+// const fetchYearData = async (selection) => {
+//   await axios.get(
+//     `https://streetsmarts-labs24.herokuapp.com/api/year?model=${selection}`
+//   );
+// };
+
 const Dropdown = () => {
-  const [make, setMake] = useState({});
-  const [makeS, setMakeS] = useState([]);
-  const [model, setModel] = useState({});
-  const [modelS, setModelS] = useState([]);
-  const [year, setYear] = useState({});
-  const [yearS, setYearS] = useState([]);
+  const [carMakes, setCarMakes] = useState([]);
+  const [makeSelected, setMakeSelected] = useState("");
 
+  const [carModels, setCarModels] = useState([]);
+  // const [modelSelected, setModelSelected] = useState("");
 
-  useEffect(async() => {
-    const response = await fetch('https://streetsmarts-labs24.herokuapp.com/api/make');
-    const json = await response.json();
-    setMakeS(json);
+  // const [carYears, setCarYears] = useState([]);
+  // const [yearSelected, setYearSelected] = useState('');
+
+  console.log("This is carMakes state", carMakes);
+  console.log("This is carModels state", carModels);
+  // console.log("this is carYears state", carYears);
+
+  const handleMakeChanges = (selected) => {
+    console.log("makeSelect", selected);
+    setMakeSelected(selected);
+  };
+
+  const handleModelChanges = (modelSelect) => {
+    console.log(modelSelect);
+  };
+
+  useEffect(() => {
+    axios
+      .get(`https://streetsmarts-labs24.herokuapp.com/api/make`)
+      .then((res) => {
+        console.log("useEffect res for makes", res);
+        setCarMakes(res.data);
+      })
+      .catch((err) => {
+        console.log("This is error in useEffect for makes", err);
+      });
   }, []);
-  useEffect(async() => {
-    const response = await fetch(`https://streetsmarts-labs24.herokuapp.com/api/model?make=${model}`);
-    const json = await response.json();
-    setModelS(json);
-  }, [make]);
-  useEffect(async() => {
-    const response = await fetch(`https://streetsmarts-labs24.herokuapp.com/api/year?model=${year}`);
-    const json = await response.json();
-    setYearS(json);
-  }, [model]);
-  
-  const click = e => {
-    e.preventDefault();
-  }
 
-  const handleChangeMake = e => {
-    setMake(e.target);
-    console.log('EVENT', e.target)
-  }
+  useEffect(() => {
+    if (makeSelected !== "") {
+      fetchModelData(makeSelected)
+        .then((res) => {
+          console.log("useEffect response for models", res);
+          setCarModels(res.data);
+        })
+        .catch((err) => {
+          console.log("This is error in useEffect for models", err);
+        });
+    }
+  }, [makeSelected]);
 
-  const handleChangeModel = e => {
-    setModel(e.target.value);
-  }
-  
-  const handleChangeYear = e => {
-    setYear(e.target.value);
-  }
-
+  // useEffect(async() => {
+  //   const response = await fetch(`https://streetsmarts-labs24.herokuapp.com/api/year?model=${makeSelected}`);
+  //   const data = await response.json();
+  //   console.log('data');
+  // }, [])
   return (
     <>
-      <form onClick={click}>
       <Select
-        id="make"
         defaultValue="Make"
-        value={make}
         style={{ width: 120 }}
-        onSelect={handleChangeMake}>
-          {makeS.map((maker) => (
+        onSelect={handleMakeChanges}>
+          {carMakes.map((maker) => (
             <Option key={maker.make} value={maker.make}>{maker.make}</Option>
           ))}
       </Select>
 
       <Select
-        id="model"
+
         defaultValue="Model"
-        value={model}
         style={{ width: 120 }}
-        onSelect={handleChangeModel}>
-          {modelS.map((modeler) => (
+        onSelect={handleModelChanges}>
+          {carModels.map((modeler) => (
             <Option key={modeler.model} value={modeler.model}>{modeler.model}</Option>
           ))}
       </Select>
 
-      <Select
-        id="year"
+      {/* <Select
         defaultValue="Year"
-        value={year}
         style={{ width: 120 }}
         onSelect={handleChangeYear}>
           {yearS.map((yearer) => (
             <Option key={yearer.year} value={yearer.year}>{yearer.year}</Option>
           ))}
-      </Select>
-      </form>
+      </Select> */}
     </>
   );
 };
