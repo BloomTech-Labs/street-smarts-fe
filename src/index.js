c0067582a224371ba100a105d5ea84e0ea34983b
unknown <bwardwell79@gmail.com>
2020-05-21 14:57:06 -0700

    changed where Router waswrapping the app to Appl.js

--- src/index.js
+++ src/index.js
@@ -6,10 +6,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Router>
-      <App />
-  </Router>,
-  document.getElementById('root')
+      <App />, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
