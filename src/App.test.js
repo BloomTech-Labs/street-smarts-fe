0d25cc441670d5e54a1982d1f02ad24ca729d706
unknown <bwardwell79@gmail.com>
2020-05-20 17:09:32 -0700

    ran initial render crash test, deleted context

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import Item from 'antd/lib/list/Item';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+test('renders without crashing', () => {
+  render(
+    <span className="greet">hello world</span>
+  )
+})
+
+
