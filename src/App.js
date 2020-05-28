f974dbbc9f2fa291a0844f94a499c67dfa5e767c
Issac Moreno <issaxavier909@gmail.com>
2020-05-27 20:28:21 -0700

    Forgot to save this change

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import "./App.css";
 
 import CarDetails from "./components/details";
 import Dropdown from "./components/search";
-import Results from "./components/results";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Route exact path = '/' component = {Dropdown} />
-        <Route exact path="/:make/:model/:year" component={Results} />
         <Route exact path="/details/:make/:model/:id" component={CarDetails} />
       </div>
     </Router>
