3690cb03191dbfa2432c757a363f07eb5fcdfbfa
hsisco <hysensisco@gmail.com>
2020-05-28 14:46:48 -0400

    Styling: App, CarDetails

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./App.css";
-import CarDetails from "./components/details";
-import Dropdown from "./components/search";
-import Results from "./components/results";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import AppStyle from './AppStyle';
+import CarDetails from './components/details';
+import Dropdown from './components/search';
 import { Layout, Menu } from 'antd';
-import styled from 'styled-components';
-
-const AppStyle = styled.div`
-  .header{
-    display: flex;
-    position: 'fixed';
-    z-index: 1;
-    width: '100%';
-    justify-content: flex-end;
-  }
-
-`;
-
-
-
-
 
 const { Header, Content, Footer } = Layout;
 
@@ -28,68 +11,31 @@ function App() {
   return (
     <Router>
       <AppStyle>
-        <Layout>
+        <Layout className='layout'>
 
           <Header className='header'>
-            <div className="logo" />
-            <Menu className='menu' mode="horizontal">
-              <Menu.Item key="1">Home</Menu.Item>
-              <Menu.Item key="2">Search</Menu.Item>
-              <Menu.Item key="3">What is CO2</Menu.Item>
-              <Menu.Item key="3">Contact</Menu.Item>
+            <div className='logo' />
+            <Menu className='menu' mode='horizontal'>
+              <Menu.Item key='1'>HOME</Menu.Item>
+              <Menu.Item key='2'>SEARCH</Menu.Item>
+              <Menu.Item key='3'>WHAT IS CO<sub>2</sub></Menu.Item>
+              <Menu.Item key='4'>CONTACT</Menu.Item>
             </Menu>
           </Header>
 
-          <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
-            <div className="AppMain" style={{ padding: 24, minHeight: 380 }}>
-        <Route exact path = '/' component = {Dropdown} />
-        <Route exact path="/details/:make/:model/:id" component={CarDetails} />
-    <Route exact path="/:make/:model" component={Results} />
-    
-              <Route exact path="/:make/:model/:year" component={Results} />
-              <Route exact path="/details/:make/:model/:id" component={CarDetails} />
-            </div>
+          <Content className='content'>
+            <Route exact path = '/' component = {Dropdown} />
+            <Route exact path='/details/:make/:model/:id' component={CarDetails} />
           </Content>
 
-          <Footer style={{ textAlign: 'center' }}>
-            Streetsmarts ©2020
+          <Footer className='footer'>
+            <p>Streetsmarts ©2020</p>
           </Footer>
         
         </Layout>
       </AppStyle>
-
     </Router>
   );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="App">
-//         <Dropdown />
-//         <Route exact path="/:make/:model" component={Results} />
-//         <Route exact path="/:make/:model/:year" component={Results} />
-//         <Route exact path="/details/:make/:model/:id" component={CarDetails} />
-//       </div>
-//     </Router>
-//   );
-// }
