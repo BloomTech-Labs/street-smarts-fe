1180e0908eb47c40402dea9c1279e69abeaccaa6
hsisco <hysensisco@gmail.com>
2020-05-29 16:49:33 -0400

    Cost to Own, renaming Dropdown

---
+++ src/components/dropdown/styles.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const DropdownStyled = styled.div`
+  .dropdown {
+    display: flex;
+    width: 1000px;
+    border: solid black 1px;
+    cursor: pointer;
+  }
+`;
+export default DropdownStyled;
---
+++ src/components/results/styles.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ResultsStyled = styled.div`
+  .resultsCard {
+    width: 500px;
+  }
+`;
+
+export default ResultsStyled;
