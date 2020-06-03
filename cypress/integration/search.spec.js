f1428156de81633d9924949681c771ae6eb38d8c
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-06-03 16:18:16 -0600

    Add test for loading details from url

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -3,19 +3,19 @@ describe("Dropdown Test Suite", () => {
     cy.visit("");
 
     cy.contains(/make/i).click();
-    cy.get('.ant-select-item-option-content').contains(/acura/i).click();
+    cy.get(".ant-select-item-option-content").contains(/acura/i).click();
 
     cy.contains(/model/i).click();
-    cy.get('.ant-select-item-option-content').contains(/mdx/i).click();
+    cy.get(".ant-select-item-option-content").contains(/mdx/i).click();
 
     cy.contains(/year/i).click();
-    cy.get('.ant-select-item-option-content').contains(/2001/i).click();
+    cy.get(".ant-select-item-option-content").contains(/2001/i).click();
 
     //resultsCard:first uses the CSS selector to pick the first result of user's search
-    cy.get('.resultsCard:first').click();
-    cy.url().should('include', '/details')
+    cy.get(".resultsCard:first").click();
+    cy.url().should("include", "/details");
   });
-  
+
   it("USer can type to select items in dropdowns", () => {
     // User can select Make value
     cy.visit("");
@@ -26,5 +26,12 @@ describe("Dropdown Test Suite", () => {
     // get the first item in the results list
     cy.get(".resultsCard:first").click();
     cy.url().should("include", "/details");
+  });
+
+  it("Visiting a url displays the car's details", () => {
+    // User can select Make value
+    cy.visit("/details/Acura/MDX/17191");
+    cy.contains(/co2 emissions/i);
+    cy.contains(/cost of ownership/i);
   });
 });
