08d9f4d80dde81a86518e8862b178c06b28eb06a
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-06-03 16:17:27 -0600

    Add test for typing in a car's details

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -10,9 +10,21 @@ describe("Dropdown Test Suite", () => {
 
     cy.contains(/year/i).click();
     cy.get('.ant-select-item-option-content').contains(/2001/i).click();
+
     //resultsCard:first uses the CSS selector to pick the first result of user's search
     cy.get('.resultsCard:first').click();
     cy.url().should('include', '/details')
   });
   
+  it("USer can type to select items in dropdowns", () => {
+    // User can select Make value
+    cy.visit("");
+    cy.contains(/make/i).type("Acura{enter}");
+    cy.contains(/model/i).type("MDX{enter}");
+    cy.contains(/year/i).type("2001{enter}");
+
+    // get the first item in the results list
+    cy.get(".resultsCard:first").click();
+    cy.url().should("include", "/details");
+  });
 });
