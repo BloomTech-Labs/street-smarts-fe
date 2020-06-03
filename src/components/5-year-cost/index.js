78fbff7193d7579abab580c8aae6f17e271e17c3
Issac Moreno <issaxavier909@gmail.com>
2020-06-03 02:40:17 -0700

    fixed classNames to represent Figma

--- src/components/5-year-cost/index.js
+++ src/components/5-year-cost/index.js
@@ -34,26 +34,26 @@ const CostToOwn = () => {
             </div>
             <div className = 'mainContainer'>
                 <h1>Cost Breakdown</h1>
-                <div className = 'row'>
-                    <div className = ''>
+                <div className = 'row top'>
+                    <div className = 'text-block'>
                         <h3>Purchase Price</h3>
                         <p>${purchasePrice.toLocaleString(undefined, {maximumFractionDigits: 2})}</p>
                     </div> 
-                    <div>
+                    <div className = 'text-block'>
                         <h3>Cost per Year</h3>
                         <p>${yearlyTotalCost.toLocaleString(undefined, {maximumFractionDigits: 2})}</p> 
                     </div>
-                    <div>
+                    <div className = 'text-block'>
                         <h3>5-Year Cost to Own</h3>
                         <p>${totalCost.toLocaleString(undefined, {maximumFractionDigits: 2})}</p>
                     </div>
                 </div>
-                <div className = 'row top'>
-                    <div>
+                <div className = 'row bottom'>
+                    <div className = 'text-block'>
                         <h3>Cost of Gas per year</h3>
                         <p>${yearlyGasSpend.toLocaleString(undefined, {maximumFractionDigits: 2})}</p>
                     </div> 
-                    <div>
+                    <div className = 'text-block'>
                         <h3>Cost of Maintenance per year</h3>
                         <p>${yearlyMaintenanceCost.toLocaleString(undefined, {maximumFractionDigits: 2})}</p>
                     </div>
