28982bcc6014f65259473419f8c068284457bbe0
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-06-08 16:19:24 -0600

    Style compare page

---
+++ src/components/compare/styles.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const CompareCarsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-gap: 1%;
+`;
+
+export const BreakdownCard = styled.div``;
