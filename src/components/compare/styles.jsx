9cb5b9db4d08acd19d804d8395ed230a3f1eb589
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-06-09 11:36:08 -0600

    Add carbon gauge to compare page

--- src/components/compare/styles.jsx
+++ src/components/compare/styles.jsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
+import HorizontalGauge from "../common/gauge";
 
 export const CompareCarsContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-  grid-template-rows: 30vh repeat(2, auto);
+  grid-template-rows: 30vh repeat(3, auto);
   grid-auto-flow: column;
   gap: 4%;
   justify-items: center;
@@ -16,4 +17,9 @@ export const BreakdownCard = styled.div`
 export const CarImg = styled.img`
   max-width: 100%;
   max-height: 100%;
+`;
+
+export const CarbonGauge = styled(HorizontalGauge)`
+  width: 250px;
+  height: 75px;
 `;
