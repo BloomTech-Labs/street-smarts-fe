cbc41c57f0e5fb96d5a15534b83c40adfbd53467
Brian Wardwell <bwardwell79@gmail.com>
2020-06-09 10:32:03 -0700

    finished 5 year styling based on Figma

--- src/components/common/breakdown/styles.jsx
+++ src/components/common/breakdown/styles.jsx
@@ -6,16 +6,32 @@ export const Wrapper = styled.div`
 
 export const BreakdownAttributes = styled.div`
 
-    width: 60vw;
+    width: 30vw;
+    
+    .text-block-top {
+        width: 10vw;
+    }
+
+    .text-block-bottom {
+
+}
 
     .row-top {
-        margin-bottom: 2vh;
+        margin-bottom: 8vh;
         display: flex;
-        justify-content: space-around;
+        justify-content: space-evenly;
+        text-align: center;
+        vl {
+      border-left: 1px solid white;
+      height: 50px;
+      position: absolute;
+      margin: 0;
+    }
      }
 
     .row-bottom {
         display: flex;
-        justify-content: space-around;
+        justify-content: space-evenly;
+        text-align: center;
     }
 `;
