7f0af0eedbd3189e32e9efc007e3de35aff65897
Issac Moreno <issaxavier909@gmail.com>
2020-06-22 14:33:21 -0700

    pulled from master

--- src/components/details/styles.jsx
+++ src/components/details/styles.jsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 const CarDetailsStyles = styled.div`
-  .car-details {
-    width: 70vw;
+  .carDetails {
+    width: 100%;
     display: flex;
     justify-content: space-between;
     background-color: rgb(0,0,0);
@@ -33,7 +33,7 @@ const CarDetailsStyles = styled.div`
       }
     }
 
-    .car-text {
+    .carText {
       width: 45%;
       background-color: rgba(0,0,0,0);
       border: none;
@@ -44,14 +44,11 @@ const CarDetailsStyles = styled.div`
       }
     }
 
-    .car-img {
+    .carImg {
       width: 45%;
       background-color: rgba(0,0,0,0);
       border: none;
       padding: 0px;
-      display: flex;
-      justify-content: center;
-      text-align: center;
 
       @media (max-width: 1100px) {
         width: 50%;
@@ -63,21 +60,9 @@ const CarDetailsStyles = styled.div`
       rect {
         width: 100%
       }
+
       .cto {
         font-size: 2rem;
-      }
-      .compare-button {
-        margin: 20%;
-      }
-      .eco-driving-link {
-        width: 25vw;
-        border: 2px solid #25D0C6;
-        border-radius: 25px;
-        margin: 10vw;
-        margin-top: 25vh;
-        a {
-          margin: 5%;
-        }
       }
     }
   }
@@ -103,22 +88,18 @@ const CarDetailsStyles = styled.div`
     }
   }
   .compare-button {
-    margin: 20%;
+    margin:20%;
     text-align: center;
   }
 
   .trees {
-    display: flex;
+    display:flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     p {
       text-align: center;
     }
-  }
-  .disclaimer-container {
-    position: absolute;
-    width: 100%;
   }
 
 `;
